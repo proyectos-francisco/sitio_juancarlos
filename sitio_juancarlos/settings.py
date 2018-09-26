e63ba03c6e21089cde514d0f170ef950129e8195
Francisco Castillo <john.kippur@gmail.com>
2018-09-26 11:21:52 -0300

    Cambios en settings.

--- sitio_juancarlos/settings.py
+++ sitio_juancarlos/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['castillosoft.pythonanywhere.com']
 
 # Application definition
 
@@ -122,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -133,5 +132,5 @@ STATICFILES_FINDERS = [
 
 SASS_PROCESSOR_ENABLED = True
 
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
